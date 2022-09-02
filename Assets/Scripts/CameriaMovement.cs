fde2c7de226ed4b928173e7243e3fe7573957225
nonereaper <111899116+nonereaper@users.noreply.github.com>
2022-09-02 17:09:48 -0400

    update

--- Assets/Scripts/CameriaMovement.cs
+++ Assets/Scripts/CameriaMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameriaMovement : MonoBehaviour
 {
-    private double angleH, angleV;
+    private float angleV;
     private float oriX, oriY;
     private Transform tf;
     // Start is called before the first frame update
@@ -13,7 +13,6 @@ public class CameriaMovement : MonoBehaviour
         tf = GetComponent<Transform>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = true;
-        angleH = 0;
         angleV = 0;
         oriX = Screen.width/2;
         oriY = Screen.height/2;
@@ -24,8 +23,14 @@ public class CameriaMovement : MonoBehaviour
     {
         
         float difX = Input.GetAxis("Mouse X") * Time.deltaTime * 100f, difY = Input.GetAxis("Mouse Y") * Time.deltaTime * 100f;
-        //Debug.Log(Input.GetAxis("Mouse X") + ",  " +Input.GetAxis("Mouse Y") + "), (" + difX + ", " + difY);
         GetComponentInParent<PlayerMovement>().rotatePlayer(difX);
-       // tf.RotateAround(tf.position,Vector3.right,difY);
+        angleV -= difY;
+        if (angleV >= 90.0) {
+            angleV = 90.0f;
+        }
+        if (angleV <= -90.0) {
+            angleV = -90.0f;
+        }
+        tf.localRotation = Quaternion.Euler(angleV,0,0);
     }
 }
