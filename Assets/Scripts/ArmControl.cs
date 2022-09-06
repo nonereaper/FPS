a2913863bcdfe98323bf22127a14a65cf94f1b12
nonereaper <111899116+nonereaper@users.noreply.github.com>
2022-09-06 17:09:21 -0400

    update

--- Assets/Scripts/ArmControl.cs
+++ Assets/Scripts/ArmControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ArmControl : MonoBehaviour
 {
+    [SerializeField] GameObject proj;
+    private float projVelocity;
     private PlayerMovement parentClass;
     private Transform tf;
 
@@ -13,6 +15,7 @@ public class ArmControl : MonoBehaviour
     {
         tf = GetComponent<Transform>();
         parentClass = GetComponentInParent<PlayerMovement>();
+        projVelocity = 700f;
     }
 
     // Update is called once per frame
@@ -22,5 +25,10 @@ public class ArmControl : MonoBehaviour
         double changeX = Math.Cos(tempAngle)*0.25, changeY = Math.Sin(tempAngle)*0.25;
         tf.localPosition = new Vector3(tf.localPosition.x,(float)(changeY+0.25f),(float)changeX);
         tf.localRotation = Quaternion.Euler(parentClass.getCameriaAngle()-90,tf.localRotation.y,tf.localRotation.z);
+        if (Input.GetButtonDown("Fire1")) {
+            GameObject o = Instantiate(proj,new Vector3(tf.position.x+10f,tf.position.y,tf.position.z),
+            Quaternion.Euler(parentClass.getCameriaAngle()-90,tf.rotation.y,tf.rotation.z));
+            o.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, projVelocity,0));
+        }
     }
 }
